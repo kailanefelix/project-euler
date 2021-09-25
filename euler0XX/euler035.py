8fdf81c716dfa2c48705a6c80c908b708581001a
Kailane Felix <kailanefelx@gmail.com>
2021-09-25 11:16:37 -0300

    add euler035 solution

---
+++ euler0XX/euler035.py
@@ -0,0 +1,17 @@
+from primality import is_prime
+import time
+
+def rotations(s: str):
+    for i in range(1, len(s)):
+       yield s[i:] + s[:i]
+
+start = time.process_time()
+
+count = 4
+for number in range(11, 1000000, 2):
+    if is_prime(number) and all(is_prime(int(rotation)) for rotation in rotations(str(number))):
+        count = count + 1
+
+elapsed_time = time.process_time() - start
+
+print(count, f'{elapsed_time * 1000:.3f} ms')
