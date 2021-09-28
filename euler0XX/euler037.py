11a8cea2d681267359a03d8ebaaeb72a78d56714
Kailane Felix <kailanefelx@gmail.com>
2021-09-28 20:32:36 -0300

    add euler037 solution

---
+++ euler0XX/euler037.py
@@ -0,0 +1,22 @@
+import itertools
+from euler0xx.primality import is_prime
+
+count, total = 0, 0
+numbers = itertools.count(11, step=2)
+
+
+def truncated(n: int):
+    s = str(n)
+    for i in range(1, len(s)):
+        yield int(s[i:])
+        yield int(s[:-i])
+
+
+while count < 11:
+    number = next(numbers)
+
+    if is_prime(number) and all(is_prime(n) for n in truncated(number)):
+        count = count + 1
+        total = total + number
+
+print(total)
